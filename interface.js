d4305f96ea0953df257d403cffcf1e58164cc8fd
heerucan <iwpow0307@gmail.com>
2022-04-02 17:09:02 +0900

    [Add] javascript로 파일 변환(#1)

---
+++ interface.js
@@ -0,0 +1,29 @@
+var serverPart = {
+    name: '김후릐',
+    age: 98,
+    group: 'yb',
+    mbti: ['ISFP', 'iOS']
+};
+console.log(serverPart);
+var serverMembers = [
+    {
+        name: '유지원',
+        age: 2500,
+        group: 'ob',
+        mbti: ['isfj', 'omoolen', 'server']
+    },
+    {
+        name: '구건모',
+        age: 4800,
+        group: 'ob',
+        mbti: ['isfj', 'server']
+    }
+];
+console.log(serverMembers);
+var closet = [
+    {
+        name: '후릐의 옷장',
+        shirt: 3
+    }
+];
+console.log(closet);
