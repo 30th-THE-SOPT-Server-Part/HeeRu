511f4a6fd8899374b70d26b129c44dd92d0b9ca1
heerucan <iwpow0307@gmail.com>
2022-04-16 13:29:34 +0900

    [Chore] request param 코드 리뷰 반영(#5)

--- assignment2/src/api/routes/blog/index.ts
+++ assignment2/src/api/routes/blog/index.ts
@@ -2,10 +2,6 @@ import express, { Router } from "express";
 
 const router: Router = express.Router();
 
-// 나 이런식으로 request param을 써주고 싶은데
-// 최종적인 url이 api/blog/:postID/like 이랬으면 좋겠는데. 안되더라?
-// router.use("/:postID/like", require("./like"));
-
-router.use("/like", require("./like"));
+router.use("/like/:postId", require("./like"));
 
 module.exports = router;
